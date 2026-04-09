960cc8130615f03c7400fe399b4de44b3f84ddfd
shayna brower <shayna.brower@colorado.edu>
2026-04-08 19:59:12 -0600

    Task 3

--- Main.m
+++ Main.m
@@ -55,7 +55,7 @@ for i = 5:N_ref
     end
 end
 
-% solve for cl_values after min panels iterating boundary points by 10 every time
+% solve for cl_values after min panels iterating boundary points by 20 every time
 for i = min_number_of_boundary_points_per_surface + 20:20:N_ref
     a = a+1;
     [x_b_0012_temp,y_b_0012_temp] = NACA_Airfoils(NACA_0012,1,i,'NACA 0012',0);
@@ -85,10 +85,54 @@ title("Convergence of Predicted Lift Coefficient with Respect to Total Number of
 
 %% Task 3
 
+NACA_0006 = [0,0,6];
+NACA_0012 = [0,0,12];
+NACA_0018 = [0,0,18];
+
+% use number of boundary points found in task 2
+N_3 = min_number_of_boundary_points_per_surface; 
+
+% create a vector for angle of attack values
+angle_of_attack = -16:1:20; %degrees
+
+[x_b_0006,y_b_0006] = NACA_Airfoils(NACA_0006,1,N_3,'NACA 0006',0);
+[x_b_0012,y_b_0012] = NACA_Airfoils(NACA_0012,1,N_3,'NACA 0012',0);
+[x_b_0018,y_b_0018] = NACA_Airfoils(NACA_0018,1,N_3,'NACA 0018',0);
+
+% solve for cl with respect to alpha for all three airfoils
+for i=1:length(angle_of_attack)
+    cl_0006(i) = Vortex_Panel(x_b_0006,y_b_0006,1,angle_of_attack(i));
+    cl_0012_task3(i) = Vortex_Panel(x_b_0012,y_b_0012,1,angle_of_attack(i));
+    cl_0018(i) = Vortex_Panel(x_b_0018,y_b_0018,1,angle_of_attack(i));
+end
 
 
+% Thin airfoil theory
+% dz/dx = 0 since all three are not cambered airfoils, so cl = 2*pi*angle of attack
+% cl slope is the same for all three airfoils 
+angle_of_attack_radians = (pi.*angle_of_attack)./180; % convert to radians
+cl_thin_airfoil = 2*pi.*angle_of_attack_radians;
+
+% experimental data from NACA charts and digitizer
+data_0006 = readmatrix("NACA0006_exp.txt");
+data_0012 = readmatrix("NACA0012_exp.txt");
 
 
+% plot cl vs aoa for all three methods
+figure;
+plot(angle_of_attack,cl_0006,'LineWidth',1.2,'Color','m');
+hold on;
+plot(data_0006(:,1),data_0006(:,2),'--om');
+plot(angle_of_attack,cl_0012_task3,'LineWidth',1.2,'Color','c');
+plot(data_0012(:,1),data_0012(:,2),'--oc');
+plot(angle_of_attack,cl_0018,'LineWidth',1,'Color','y');
+plot(angle_of_attack,cl_thin_airfoil,'--w','LineWidth',1.2);
+grid on;
+legend("NACA 0006 Vortex Panel","NACA 0006 Experimental","NACA 0012 Vortex Panel","NACA 0012 Experimental","NACA 0018 Vortex Panel","Thin Airfoil Theory","Location","southeast");
+xlabel("Angle of Attack (degrees)");
+ylabel("Lift Coefficient");
+title("Lift Coefficient vs Angle of Attack for Airfoils of Various Thickness");
+print("thickness comparison","-dpng",'-r300');
 
 
 
